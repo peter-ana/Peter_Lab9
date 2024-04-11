e46d09a4b8c48b134a57b703122d2f65f9ecab3f
unknown <r.podder@ufl.edu>
2024-04-11 13:12:19 -0400

    partner addition to repo

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def main():
+    print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit""")
+    choice = input("Please enter an option: ")
+    if choice == 1:
+        password = input("Please enter your password to encode: ")
+        encoded_password = ""
+        for digit in password:
+            encoded_digit = str((int(digit) + 3) % 10)
+            encoded_password += encoded_digit
+        print("Your password has been encoded and stored!")
+    if choice == 2:
+	decoded_password = ""
+	for i in password:
+		decoded_digit=str((int(i)-3)%10)
+		decoded_password += decoded_digit
+        print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+	
+    if choice == 3:
+	exit()
+
