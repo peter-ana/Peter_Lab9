7022442cf860f04d8edd8cfa19a04c495397cc86
r-podder <166727472+r-podder@users.noreply.github.com>
2024-04-11 13:18:31 -0400

    Lab_9.py

--- Lab_9.py
+++ Lab_9.py
@@ -4,7 +4,7 @@ def main():
 1. Encode
 2. Decode
 3. Quit""")
-    choice = input("Please enter an option: ")
+    choice = int(input("Please enter an option: "))
     if choice == 1:
         password = input("Please enter your password to encode: ")
         encoded_password = ""
@@ -12,6 +12,12 @@ def main():
             encoded_digit = str((int(digit) + 3) % 10)
             encoded_password += encoded_digit
         print("Your password has been encoded and stored!")
-    if choice == 2:
+    elif choice == 2:
+	    decoded_password = ""
+	    for i in password:
+		    decoded_digit=str((int(i)-3)%10)
+		    decoded_password += decoded_digit
         print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
     if choice == 3:
+	    exit()
